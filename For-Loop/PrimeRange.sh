5549dd0e8be2f4f2726190464e8e39fcdc4a76e4
SurabhiBorase <surabhiborase44@gmail.com>
2022-02-04 02:15:44 +0530

    ADDED: Display Range Prime Number by For-Loop

---
+++ For-Loop/PrimeRange.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Extend the program to take a range of number as input and output the Prime Numbers in that range.
+
+read -p "Enter the Starting Range Value: " start
+read -p "Enter the Ending Range Value: " end
+
+temp=1;
+while(( $start<=$end ))
+do
+	if(( $start!=1 ))
+	then
+		for(( i=2; $i<$start; i++ ))
+		do
+			if(( $start%$i==0 ))
+			then
+				temp=0;
+				break;
+			fi
+		done
+	fi
+	if(( $temp==1 ))
+	then
+		echo $start " Number is Prime";
+	else
+		temp=1;
+	fi
+	((start++));
+done
